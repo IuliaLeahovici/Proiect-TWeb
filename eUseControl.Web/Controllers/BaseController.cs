708dbed7c28423826a6c13e8b813973910e5e12c
unknown <iulia.leahovici@isa.utm.md>
2025-05-02 15:30:25 +0300

    Cleaned up submodule

---
+++ eUseControl.Web/Controllers/BaseController.cs
@@ -0,0 +1,45 @@
+﻿using eUseControl.BussinesLogic.Interfaces;
+using eUseControl.BussinesLogic;
+using eUseControl.Web.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eUseControl.Web.Controllers
+{
+    public class BaseController : Controller
+    {
+          private readonly ISession _session;
+
+          public BaseController()
+          {
+               var bl = new BussinessLogic();
+               _session = bl.GetSessionBL();
+          }
+
+          public void SessionStatus()
+          {
+               var apiCookie = Request.Cookies["X-KEY"];
+               if (apiCookie != null)
+               {
+                    var profile = _session.GetUserByCookie(apiCookie.Value);
+                    if (profile != null)
+                    {
+                         System.Web.HttpContext.Current.SetMySessionObject(profile);
+                         System.Web.HttpContext.Current.Session["LoginStatus"] = "login";
+                    }
+                    else
+                    {
+                         System.Web.HttpContext.Current.Session.Clear();
+                         System.Web.HttpContext.Current.Session["LoginStatus"] = "guest";
+                    }
+               }
+               else
+               {
+                    System.Web.HttpContext.Current.Session["LoginStatus"] = "guest";
+               }
+          }
+     }
+}
---
+++ eUseControl.Web/Models/EditUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eUseControl.Web.Models
+{
+     public class EditUser
+     {
+          public string Username { get; set; }
+
+          public string Password { get; set; }
+
+          public string Email { get; set; }
+
+
+          public string Image { get; set; }
+     }
+}
